24f45d3ad85bc8b57e739d961e65173954571b4f
Gabriel Wersebe <gwersebe1@gmail.com>
2023-11-25 11:11:27 -0500

    Initial Commit

---
+++ Interval Timer/Interval Timer Watch App/Interval_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_TimerApp.swift
+//  Interval Timer Watch App
+//
+//  Created by Gabriel Wersebe on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Interval_Timer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
