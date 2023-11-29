ea86df84b6a3fd64b2a1bea8b16d117da11f655a
Gabriel Wersebe <gwersebe1@gmail.com>
2023-11-28 20:23:13 -0500

    Created MVP

--- Interval Timer Watch App/ContentView.swift
+++ Interval Timer Watch App/ContentView.swift
@@ -7,18 +7,47 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var selectedMinutes: Int = 1
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Picker("Minutes", selection: $selectedMinutes) {
+                    ForEach(0 ..< 60) { index in
+                        Text("\((index + 1)) min")
+                            .tag(index + 1)
+                    }
+                }
+                .labelsHidden()
+                .frame(width: 100)
+                .padding()
+                
+                NavigationLink(destination: CountdownView(totalTime: $selectedMinutes)) {
+                    Text("Start")
+                        .padding()
+                }
+                .disabled(selectedMinutes <= 0)
+                .opacity(selectedMinutes <= 0 ? 0.5 : 1)
+                .onTapGesture {
+                    if selectedMinutes <= 0 {
+                        print("Please select a valid time greater than 0.")
+                    }
+                }
+            }
+            .padding()
+            .navigationBarTitle("Interval Timer")
+            .navigationBarTitleDisplayMode(.large)
+
+            
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
--- Interval Timer Watch App/CountdownView.swift
+++ Interval Timer Watch App/CountdownView.swift
@@ -1,83 +1,206 @@
 //
-//  CountdownTimer.swift
+//  CountdownView.swift
 //  Interval Timer Watch App
 //
 //  Created by Gabriel Wersebe on 11/25/23.
 //
 
+
 import SwiftUI
+import WatchConnectivity
+import WatchKit
 
-struct CountdownView: View {
-    @State private var timer: Timer?
-    @State private var remainingTime: TimeInterval = 300
-    @State private var isRunning = false
 
-    var body: some View {
-        VStack {
-            Text("\(formattedTime(remainingTime))")
-                .font(.system(size: 40))
-                .padding()
 
-            HStack {
-                Button(action: {
-                    startTimer()
-                }) {
-                    Text("Start")
-                }
-                .padding()
+class CountdownManager: NSObject, ObservableObject {
+    static let shared = CountdownManager()
 
-                Button(action: {
-                    stopTimer()
-                }) {
-                    Text("Stop")
-                }
-                .padding()
+    @Published var remainingTime: TimeInterval = 0
+    @Published var originalTime: TimeInterval = 0
+    @Published var isRunning = false
+    var session: WKExtendedRuntimeSession?
+    var timer: Timer?
 
-                Button(action: {
-                    resetTimer()
-                }) {
-                    Text("Reset")
-                }
-                .padding()
-            }
-        }
-        .onDisappear {
-            stopTimer()
-        }
+    private override init() {
+        super.init()
+        restoreTimerState()
     }
 
+    func saveTimerState() {
+        UserDefaults.standard.set(remainingTime, forKey: "RemainingTime")
+        UserDefaults.standard.set(isRunning, forKey: "IsRunning")
+    }
+
+    func restoreTimerState() {
+        if let savedRemainingTime = UserDefaults.standard.value(forKey: "RemainingTime") as? TimeInterval {
+            remainingTime = savedRemainingTime
+        }
+
+        if let savedIsRunning = UserDefaults.standard.value(forKey: "IsRunning") as? Bool {
+            isRunning = savedIsRunning
+        }
+    }
+    
+    
     func startTimer() {
-        if !isRunning {
-            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                if remainingTime > 0 {
-                    remainingTime -= 1
+        guard !isRunning else { return }
+
+        timer?.invalidate()
+        session?.invalidate()
+
+        // Check if the session is nil before creating a new one
+        if session == nil {
+            session = WKExtendedRuntimeSession()
+            session?.delegate = self
+            session?.start(at: Date())
+        }
+
+        // Ensure that the timer is scheduled on the main run loop
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] _ in
+            guard let self = self else { return }
+
+            if self.isRunning {
+                if self.remainingTime > 0 {
+                    self.remainingTime -= 1
                 } else {
-                    stopTimer()
+                    self.stopTimer()
                 }
+
+                if Int(self.remainingTime) % 60 == 0 {
+                    WKInterfaceDevice.current().play(.notification)
+                    print("Debug Vibrate")
+                }
+
+                print("Remaining Time: \(self.remainingTime)")
+                print("Timer is running in the background")
             }
-            isRunning = true
+        })
+
+        isRunning = true
+    }
+
+
+    func pauseTimer() {
+        guard isRunning else { return }
+
+        // Invalidate the timer and session
+        timer?.invalidate()
+        session?.invalidate()
+
+        // Set both timer and session to nil after invalidating
+        timer = nil
+        session = nil
+
+        isRunning = false
+        saveTimerState()
+    }
+
+
+
+
+    func toggleTimer() {
+        if isRunning {
+            pauseTimer()
+        } else {
+            startTimer()
         }
     }
 
     func stopTimer() {
         timer?.invalidate()
+        session?.invalidate()
         isRunning = false
+        remainingTime = 0
     }
 
     func resetTimer() {
         stopTimer()
-        remainingTime = 300
+        remainingTime = originalTime
+    }
+    
+    deinit {
+        // Ensure that the session is invalidated when CountdownManager is deallocated
+        if isRunning {
+            session?.invalidate()
+        }
+    }
+
+}
+
+extension CountdownManager: WKExtendedRuntimeSessionDelegate {
+    func extendedRuntimeSession(_ extendedRuntimeSession: WKExtendedRuntimeSession, didInvalidateWith reason: WKExtendedRuntimeSessionInvalidationReason, error: Error?) {
+        if let error = error {
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+
+    func extendedRuntimeSessionDidStart(_ extendedRuntimeSession: WKExtendedRuntimeSession) {
+        // Nothing needed here
+    }
+
+    func extendedRuntimeSessionDidInvalidate(_ extendedRuntimeSession: WKExtendedRuntimeSession) {
+        // Complete any necessary cleanup when the session is invalidated
+        stopTimer()
+    }
+
+    func extendedRuntimeSessionWillExpire(_ extendedRuntimeSession: WKExtendedRuntimeSession) {
+        // Handle session expiration if needed
+        stopTimer()
+    }
+}
+
+struct CountdownView: View {
+    @StateObject private var countdownManager = CountdownManager.shared
+    @Binding var totalTime: Int
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View {
+        VStack {
+            Text("\(formattedTime(countdownManager.remainingTime))")
+                .font(.system(size: 40))
+                .padding()
+
+            HStack {
+                Button(action: {
+                    countdownManager.resetTimer()
+                }) {
+                    Image(systemName: "arrowshape.turn.up.backward.circle.fill")
+                        .font(.title)
+                        .foregroundColor(.blue)
+                }
+
+                Button(action: {
+                    countdownManager.toggleTimer()
+                }) {
+                    Image(systemName: countdownManager.isRunning ? "pause.circle.fill" : "play.circle.fill")
+                        .font(.title)
+                        .foregroundColor(countdownManager.isRunning ? .yellow : .green)
+                }
+
+                Button(action: {
+                    countdownManager.resetTimer()
+                    presentationMode.wrappedValue.dismiss()
+                }) {
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.title)
+                        .foregroundColor(.red)
+                }
+            }
+        }
+        .onDisappear {
+            countdownManager.saveTimerState()
+        }
+        .onAppear {
+            countdownManager.remainingTime = Double(totalTime * 60)
+            countdownManager.originalTime = Double(totalTime * 60)
+            countdownManager.startTimer()
+        }
+        .navigationBarBackButtonHidden(true)
     }
 
     func formattedTime(_ time: TimeInterval) -> String {
         let minutes = Int(time) / 60
         let seconds = Int(time) % 60
         return String(format: "%02d:%02d", minutes, seconds)
-    }
-}
-
-struct CountdownView_Previews: PreviewProvider {
-    static var previews: some View {
-        CountdownView()
     }
 }
